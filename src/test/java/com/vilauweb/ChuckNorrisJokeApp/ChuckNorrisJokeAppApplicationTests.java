9a99ef2d7fd85d6a4f8b5dbd9b826dd95f8d1c7f
VilauGC <vilau.gc@gmail.com>
2022-04-20 21:25:27 +0300

    First commit

---
+++ src/test/java/com/vilauweb/ChuckNorrisJokeApp/ChuckNorrisJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilauweb.ChuckNorrisJokeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
